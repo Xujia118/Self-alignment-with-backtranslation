b8cae6ff2b65420c0b6c46a24cb959758c85d561
Jia Xu <xujia118@hotmail.com>
2025-11-30 18:47:17 -0800

    Modified hf home space

--- run_job_stop.sh
+++ run_job_stop.sh
@@ -5,7 +5,7 @@ USERNAME="ubuntu"
 SCRIPT_PATH="scripts.01_generate_instruction"
 
 # Set HuggingFace cache directory to persistent volume
-export HF_HOME="/runpod-volume/huggingface"
+export HF_HOME="/workspace/huggingface"
 mkdir -p "$HF_HOME"
 
 # Create a symlink for HF cache to ensure nothing writes to container disk
